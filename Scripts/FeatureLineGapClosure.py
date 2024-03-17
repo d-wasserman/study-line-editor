42249f16b091a2e0b66ff239e2def097e37e1159
David Wasserman <david.wasserman.plan@gmail.com>
2024-03-17 00:14:46 -0700

    Update FeatureLineGapClosure.py
    Update edits to fix data edits. Stuck in on FC creation. Might need SEDFs.

--- Scripts/FeatureLineGapClosure.py
+++ Scripts/FeatureLineGapClosure.py
@@ -36,58 +36,58 @@ def create_gap_filling_lines(input_line_features, output_feature_class, search_r
     - search_radius (LinearUnit): The search radius within which to identify the closest end points for gap filling.
     - connection_count(int): The number of connections to create between end points in the order of proximity. 
     """
-    try:
-        ll.arc_print("Feature class validation...")
-        arcpy.env.overwriteOutput = True
-        oid = arcpy.Describe(input_line_features).OIDFieldName
-        workspace = "in_memory"
-        pt_id, ln_id = "PT_ID" , "LINE_ID"
-        workspace, output_fc_name = os.path.split(output_feature_class)
-        arcpy.AddField_management(input_line_features, ln_id, "LONG")
-        arcpy.CalculateField_management(input_line_features, ln_id, "!{0}!".format(oid), "PYTHON3")
-        ll.arc_print("Explode the line features to their end points...")
-        end_points_temp = os.path.join(workspace,"end_points")
-        arcpy.FeatureVerticesToPoints_management(input_line_features, end_points_temp, "BOTH_ENDS")
-        
-        ll.arc_print("Add a unique identifier to each end point...")
-        arcpy.AddField_management(end_points_temp, pt_id, "LONG")
-        arcpy.CalculateField_management(end_points_temp, pt_id, "!{0}!".format(oid), "PYTHON3")
-        ll.arc_print("Construct the near table to find closest points within the search radius...")
-        near_table_temp = os.path.join(workspace,"end_points_near")
-        arcpy.GenerateNearTable_analysis(end_points_temp, end_points_temp, near_table_temp, 
-                                         search_radius, "NO_LOCATION", "NO_ANGLE", "CLOSEST")
-        ll.arc_print("Filter out points that are from the same line feature...")
-        # Load data into a DataFrame
-        df = ll.arcgis_table_to_df(end_points_temp, input_fields=["SHAPE@", pt_id,ln_id])
-        # Create a dictionary from the columns
-        sm_df = df[[ln_id,pt_id]].copy()
-        groups = sm_df.groupby(pt_id)
-        line_dict=  {pt_id: group for pt_id, group in groups}
-      
-        ll.arc_print(line_dict)
-        filtered_near_table = [row for row in arcpy.da.SearchCursor(near_table_temp, ["IN_FID", "NEAR_FID", "NEAR_DIST"]) 
-                               if line_dict[row[0]] != line_dict[row[1]]
-        
-        ll.arc_print("Create the output feature class...")
-        arcpy.CreateFeatureclass_management(workspace, output_fc_name, "POLYLINE", spatial_reference=input_line_features)
-        
-        ll.arc_print("Insert new lines into the output feature class...")
-        with arcpy.da.InsertCursor(output_feature_class, ["SHAPE@"]) as insert_cursor:
-            for row in filtered_near_table:
-                near_distance = row[2]
-                if near_distance >= search_radius:
-                    start_point = arcpy.da.SearchCursor(end_points_temp, ["SHAPE@"], "{0} = {1}".format(oid, row[0])).__next__()[0]
-                    end_point = arcpy.da.SearchCursor(end_points_temp, ["SHAPE@"], "{0} = {1}".format(oid, row[1])).__next__()[0]
-                    line = arcpy.Polyline(arcpy.Array([start_point.firstPoint, end_point.firstPoint]), 
-                                        start_point.spatialReference)
-                    insert_cursor.insertRow([line])
-                else:
-                    continue
-                
+    # try:
+    ll.arc_print("Feature class validation...")
+    arcpy.env.overwriteOutput = True
+    oid = arcpy.Describe(input_line_features).OIDFieldName
+    workspace = "in_memory"
+    pt_id, ln_id = "PT_ID" , "LINE_ID"
+    workspace, output_fc_name = os.path.split(output_feature_class)
+    arcpy.AddField_management(input_line_features, ln_id, "LONG")
+    arcpy.CalculateField_management(input_line_features, ln_id, "!{0}!".format(oid), "PYTHON3")
+    ll.arc_print("Explode the line features to their end points...")
+    end_points_temp = os.path.join(workspace,"end_points")
+    arcpy.FeatureVerticesToPoints_management(input_line_features, end_points_temp, "BOTH_ENDS")
+    
+    ll.arc_print("Add a unique identifier to each end point...")
+    arcpy.AddField_management(end_points_temp, pt_id, "LONG")
+    arcpy.CalculateField_management(end_points_temp, pt_id, "!{0}!".format(oid), "PYTHON3")
+    ll.arc_print("Construct the near table to find closest points within the search radius...")
+    near_table_temp = os.path.join(workspace,"end_points_near")
+    arcpy.GenerateNearTable_analysis(end_points_temp, end_points_temp, near_table_temp, 
+                                        search_radius, "NO_LOCATION", "NO_ANGLE", "CLOSEST")
+    ll.arc_print("Filter out points that are from the same line feature...")
+    # Load data into a DataFrame
+    df = ll.arcgis_table_to_df(end_points_temp, input_fields=["SHAPE@", pt_id,ln_id])
+    df.set_index(pt_id)
+    # Create a dictionary from the columns
+    sm_df = df[[ln_id,pt_id]].copy()
+    groups = sm_df.groupby(pt_id)
+    line_dict=  {pt_id: group[ln_id].values[0] for pt_id, group in groups}
+    
+    ll.arc_print(line_dict)
+    filtered_near_table = [row for row in arcpy.da.SearchCursor(near_table_temp, ["IN_FID", "NEAR_FID", "NEAR_DIST"]) 
+                            if line_dict[row[0]] != line_dict[row[1]]]
+    
+    ll.arc_print("Create the output feature class...")
+    arcpy.CreateFeatureclass_management(workspace, output_fc_name, "POLYLINE", spatial_reference=input_line_features)
+    a_nd, b_nd = "A_NODE", "B_NODE"
+    arcpy.AddField_management(output_feature_class, a_nd,"LONG")
+    arcpy.AddField_management(output_feature_class,b_nd,"LONG")
+    ll.arc_print("Insert new lines into the output feature class...")
+    with arcpy.da.InsertCursor(output_feature_class, ["SHAPE@",a_nd,b_nd]) as insert_cursor:
+        for row in filtered_near_table:
+            start_point = df[df[pt_id]==row[0]]["SHAPE@"].values[0]
+            end_point = df[df[pt_id]==row[1]]["SHAPE@"].values[0]
+            line = arcpy.Polyline(arcpy.Array([start_point, end_point]), 
+                                input_line_features.spatialReference)
+            insert_cursor.insertRow([line,row[0], row[1]])
+            
+            
         ll.arc_print("Gap filling lines created successfully.")
         
-    except Exception as e:
-        arcpy.AddError(str(e))
+    # except Exception as e:
+    #     arcpy.AddError(str(e))
 
 if __name__ == '__main__':
     # Define Inputs

767363808e26b88b0cb0474e0edcf323a3bc5a61
David Wasserman <david.wasserman.plan@gmail.com>
2026-03-14 23:44:36 -0700

    Update FeatureLinePull.py
    Comment, import, and doc string edits.

--- Scripts/FeatureLinePull.py
+++ Scripts/FeatureLinePull.py
@@ -1,7 +1,6 @@
 # Name: FeatureLinePull.py
 # Purpose: Take a feature class and pull back a line equal to a target distance from either a start or end point
-# position. This version of the tool will join the original fields.
-# of the old feature class.
+# position. This version of the tool will carry over the original fields of the input feature class.
 # Author: David Wasserman
 # Last Modified: 10/20/2019
 # Copyright: David Wasserman
@@ -61,7 +60,7 @@ def pull_line_geometry(
             segment_returned = linegeometry.segmentAlongLine(
                 start_point_start_position, end_point_start_position
             )
-    except:  # Should the function failr, return null geometry.
+    except Exception:  # If the function fails, return null geometry.
         return None
     return segment_returned
 
@@ -106,7 +105,7 @@ def feature_line_pull(
                     segment_rows = []
                     lineCounter += 1
                     linegeo = singleline[f_dict["SHAPE@"]]
-                    # Function splits linegeometry based on method and split value
+                    # Function retracts line geometry endpoints by the pull value
                     split_segment_geometry = pull_line_geometry(
                         linegeo,
                         fll.line_length(
@@ -117,7 +116,8 @@ def feature_line_pull(
                     )
                     if split_segment_geometry is None:
                         null_counter += 1
-                        # continue - # Uncomment to skip null geometries, otherwise empty geometries will be inserted.
+                        # Uncomment the next line to skip null geometries; by default empty geometries are inserted.
+                        # continue
                     segID = 0
                     try:
                         segID += 1
